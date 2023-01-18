5cf13a1506c8c69634f998bc72735d51b91de39f
rodrigolazcano <rodrilazcanosax@gmail.com>
2023-01-18 02:37:24 -0300

    resto de ejercicios mod 3

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,35 +3,46 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código: return "string";
+   // Tu código: 
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código: return (x+y);
+   // Tu código: 
+   return(x+y);
 }
+suma(7,8);
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código: return (x-y);
+   return(x-y);
 }
+resta(15,8);
 
 function divide(x, y) {
    // Retorna el resultado de su división.
-   // Tu código: return (x/y);
+   // Tu código:
+   return(x/y);
 }
+divide(10,5);
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
-   // Tu código: return (x*y);
+   // Tu código:
+   return(x*y);
 }
+multiplica(7,3);
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
-   // Tu código: return (x%y);
+   // Tu código:
+   return(x%y);
 }
+obtenerResto(11,2);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
